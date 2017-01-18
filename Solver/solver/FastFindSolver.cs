35f2dc2bb84dffccb674fcd6598be7a3d3fd5e2a
Mehran Kordi <mkordi@gmail.com>
2017-01-18 11:24:51 +0100

    added some comments.

--- Solver/solver/FastFindSolver.cs
+++ Solver/solver/FastFindSolver.cs
@@ -2,6 +2,18 @@
 {
     public class FastFindSolver : BaseSolver
     {
+        /// <summary>
+        /// The find operation in disjoint-set finds what partition an element belongs to
+        /// </summary>
+        /// <param name="lookup">The array that contains the data structure.</param>
+        /// <param name="node">The element to lookup</param>
+        /// <param name="size">The size of the partition element belongs to</param>
+        /// <returns>The identity of the partition the element belongs to.</returns>
+        /// <remarks>Since this is a fast find implementation there are only two cases.
+        /// either the element that we are searching for is a root itself in which case 
+        /// it has a negative value as the size of partition or it points to another index 
+        /// in the array in which case the parent is a root.
+        /// </remarks>
         protected int Find(int[] lookup, int node, out int size)
         {
             var value = lookup[node];
@@ -15,6 +27,20 @@
             size = lookup[value];
             return value;
         }
+
+        /// <summary>
+        /// This method unions two partitions in a disjoint sets. Since it's a fast find implementation
+        /// we find the root of two partitions and update every single element in the smaller partition
+        /// to point to the root of the bigger partition.
+        /// </summary>
+        /// <param name="lookup">The data structure containing the disjoint sets.</param>
+        /// <param name="fromNode">The node from first partition to join</param>
+        /// <param name="toNode">The node from second partition to join</param>
+        /// <remarks>
+        /// from and to nods can not be the same node. If both from and to point to the same partition
+        /// we have a loop in the partition and we add it to a special partition reserved for all partitions 
+        /// with loops.
+        /// </remarks>
         public override void Union(int[] lookup, int fromNode, int toNode)
         {
             int fromSize, toSize;
@@ -22,7 +48,7 @@
             var fromRoot = Find(lookup, fromNode, out fromSize);
             var toRoot = Find(lookup, toNode, out toSize);
 
-            var fullSize = fromSize + toSize;
+            var newSize = fromSize + toSize;
 
             if (fromRoot == toRoot)
             {
@@ -35,22 +61,22 @@
 
             if (fromSize <= toSize)
             {
-                lookup[fromRoot] = fullSize;
+                lookup[fromRoot] = newSize;
                 lookup[toRoot] = fromRoot;
                 if (toSize < -1)
                 {
-                    UpdateAllPointers(lookup, fromRoot, toRoot, fullSize);
+                    UpdateAllPointers(lookup, fromRoot, toRoot, newSize);
                 }
 
                 return;
             }
 
-            lookup[toRoot] = fullSize;
+            lookup[toRoot] = newSize;
             lookup[fromRoot] = toRoot;
 
             if (fromSize < -1)
             {
-                UpdateAllPointers(lookup, toRoot, fromRoot, fullSize);
+                UpdateAllPointers(lookup, toRoot, fromRoot, newSize);
             }
         }
 
--- Solver/solver/FastUnionSolver.cs
+++ Solver/solver/FastUnionSolver.cs
@@ -1,19 +1,26 @@
 ﻿namespace Solver
 {
-    using System.Runtime.CompilerServices;
-
     public class FastUnionSolver : BaseSolver
     {
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        /// <summary>
+        /// Finds the root of a partition give an element in that partition.
+        /// </summary>
+        /// <param name="lookup">The array containing the disjoint sets.</param>
+        /// <param name="node">The element to lookup.</param>
+        /// <returns>The identity of the partition the node belongs to</returns>
+        /// <remarks>
+        /// Since this is fast union implementation, the find operation is slow since it has to 
+        /// go all the way up to find the root. The path compaction technic is used to 
+        /// optimize lookup time for repeated lookups.
+        /// </remarks>
         public static int SlowFindRoot(int[] lookup, int node)
         {
             var pathCompaction = int.MaxValue;
 
-
             while (true)
             {
                 var sizeAndPointer = lookup[node];
-                if (sizeAndPointer < 0)
+                if (sizeAndPointer < 0) /*It's a root*/
                 {
                     if (pathCompaction != int.MaxValue)
                     {
@@ -24,11 +31,21 @@
                 }
 
 
-                // pathCompaction = node;
+                pathCompaction = node;
                 node = sizeAndPointer;
             }
         }
-
+        /// <summary>
+        /// Merges two partitions into one
+        /// </summary>
+        /// <param name="lookup">The array containing the data structure.</param>
+        /// <param name="fromNode">A node in the first partition.</param>
+        /// <param name="toNode">A node in the second partition.</param>
+        /// <remarks>
+        /// Since this is a fast union implementation, The merge operation just points the curent node to
+        /// the other node.To make sure the two nodes are not already in the same partition (and thus form a loop) 
+        /// we have to find their roots
+        /// </remarks>
         public override void Union(int[] lookup, int fromNode, int toNode)
         {
             var fromRoot = SlowFindRoot(lookup, fromNode);
